52e6ee9a0f58993fb5ab0b21da36506ba69f9a5e
Isaraporn Kulkumjon <ikulkumj@andrew.cmu.edu>
2011-09-24 02:29:55 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThaiSpecialDishes::Application.config.secret_token = '78f132aff75733f12b3dd8f19d446927f57e300ee9abe26977822d5baa424fbcb672c70a7697e1129731b940cc40dbb184a8a9363661ab6ef939237b580b778e'
